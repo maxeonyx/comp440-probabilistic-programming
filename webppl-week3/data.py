1f029000be1e9049a29692853a6a1e96b7647684
Maxwell Clarke <maxeonyx@gmail.com>
2021-03-11 11:39:09 +1300

    Improvements to lin regr

--- webppl-week3/data.py
+++ webppl-week3/data.py
@@ -1,9 +1,9 @@
 import numpy as np
-
+n_data = 100
 slope = 3
 intercept = 6
-x = np.linspace(0, 10, 11) + np.random.normal(0, 0.3, 11)
-y = ( x * slope + intercept ) + np.random.normal(0, 4, 11)
+x = np.linspace(0, 10, 100)
+y = ( x * slope + intercept ) + np.random.normal(0, 4, 100)
 
-print(x)
-print(y)
+print(repr(x))
+print(repr(y))
